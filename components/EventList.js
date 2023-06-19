2dd579776c005efa23f05dd995c5d49f500d6b26
karalakrepp <124396114+karalakrepp@users.noreply.github.com>
2023-06-19 22:20:50 +0300

    Update EventList.js
    Kod düzenlendi ve son event gözükmeme sorunu çözüldü

--- components/EventList.js
+++ components/EventList.js
@@ -1,29 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { updateDoc, doc } from 'firebase/firestore';
-import { db } from '../Firebase/firebase';
+import React, { useState, useEffect } from "react";
+import {
+  FlatList,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../Firebase/firebase";
 
 export default function EventList({ events }) {
   const [eventList, setEventList] = useState([]);
 
   useEffect(() => {
     if (events) {
-      const filteredEvents = events.filter(event => event.aktifMi !== false);
+      const filteredEvents = events.filter((event) => event.aktifMi !== false);
       setEventList(filteredEvents);
     }
   }, [events]);
 
   const handleDeleteEvent = async (eventId) => {
     try {
-      const eventRef = doc(db, 'events', eventId);
+      const eventRef = doc(db, "events", eventId);
       await updateDoc(eventRef, { aktifMi: false });
-      console.log('Event deleted successfully!');
+      console.log("Event deleted successfully!");
 
-      const updatedEvents = eventList.filter(event => event.id !== eventId);
+      const updatedEvents = eventList.filter((event) => event.id !== eventId);
       setEventList(updatedEvents);
     } catch (error) {
-      console.error('Error deleting event:', error);
+      console.error("Error deleting event:", error);
     }
   };
 
@@ -32,27 +39,33 @@ export default function EventList({ events }) {
       return null;
     }
 
-
-    
     return (
       <ScrollView vertical showsVerticalScrollIndicator={false}>
-      <View style={{ borderColor: '#4158D0', borderWidth: 1, borderRadius: 8, marginBottom: 8, marginHorizontal: 15, alignItems: 'center',flexDirection:'row',justifyContent:'space-between' }}>
-       <View style={{padding:10,marginLeft:10}}>
-
-       <Text style={{ fontSize: 22, color: 'black', fontWeight: 'bold' }}>Başlık: {item.title}</Text>
-        <Text style={{ fontSize: 17, color: 'black', fontWeight: '700' }}>Tarih: {item.date}</Text>
-        <Text style={{ fontSize: 17, color: 'black', fontWeight: '700' }}>Hatırlatıcı: {item.reminder}:00</Text>
-       </View>
-        <TouchableOpacity onPress={() => handleDeleteEvent(item.id)} style={{marginRight:15}}>
-          <AntDesign name="delete" size={24} color="red" />
-        </TouchableOpacity>
-      </View>
+        <View style={styles.renderContainer}>
+          <View style={{ padding: 10, marginLeft: 10 }}>
+            <Text style={styles.title}>
+              Başlık: {item.title}
+            </Text>
+            <Text style={styles.subTitle}>
+              Tarih: {item.date}
+            </Text>
+            <Text style={styles.subTitle}>
+              Hatırlatıcı: {item.reminder}:00
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => handleDeleteEvent(item.id)}
+            style={{ marginRight: 15 }}
+          >
+            <AntDesign name="delete" size={24} color="red" />
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     );
   };
 
   return (
-    <View>
+    <View style={{ marginBottom: 50 }}>
       <FlatList
         data={eventList}
         renderItem={renderItem}
@@ -62,3 +75,24 @@ export default function EventList({ events }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  renderContainer: {
+    borderColor: "#4158D0",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 8,
+    marginHorizontal: 15,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  subTitle:{
+    fontSize: 17, fontWeight: "700"
+  },
+  title:{
+    fontSize: 19, fontWeight: "bold"
+
+  }
+});
